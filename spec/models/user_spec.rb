da38f5ed337be12f0dcf329cd9bc7722a9dcc93b
V.Romanov <v.roomanov@gmail.com>
2015-11-09 02:29:17 +0400

    before rake:reset

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,11 @@ describe User do
   it { should respond_to(:microposts) }
   it { should be_valid }
   it { should_not be_admin }
+  it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
 
   
   describe "with admin attribute set to 'true'" do
@@ -142,6 +147,34 @@ describe User do
     end
   end
   
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    it{expect(@user.followed_users).to  include(other_user) }
+
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      it{expect(@user.followed_users).not_to  include(other_user) }
+      # its(:followed_users) { should_not include(other_user) }
+    end
+
+    it { should be_following(other_user) }
+    it{expect(@user.followed_users).to include(other_user) }
+    # its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      it{expect(other_user.followers).to  include(@user) }
+      # its(:followers) { should include(@user) }
+    end
+  end
   # describe "micropost associations" do
 
   #   before { @user.save }
